e2f1b781a7ce5b4ba089d20fe9f2e1d5a474db18
Razz67 <srasberry00@gmail.com>
2022-08-10 16:50:01 -0400

    Add Practice folder

---
+++ Practice/app.js
@@ -0,0 +1,125 @@
+// Extra Exercise
+// let box1 = 10;
+// let box2 = 3;
+// let temp;
+// temp = box1;
+// console.log("temp is now " + temp);
+// box1 = box2
+// console.log("box1 is now " + box1);
+// box2 = temp;
+// console.log("box2 is now " + box2);
+
+// let num1 = 25;
+// let result;
+// if (num1 >= 0 && num1 > 100) {
+//     result = "Num is positive and greater than 100";
+// } else if (num1 >= 0 && num1 < 100) {
+//     result = "Num is positive and less that 100";
+// } else {
+//     result = "num is Negative";
+// }
+//  console.log(result);
+
+// //  Grade Exercise
+
+// let grade = 88;
+// let gradeResult = "";
+// if (grade >= 90) {
+//     gradeResult = "A";
+// } else if (grade >= 80) {
+//     gradeResult = "B";
+// } else if (grade >= 70) {
+//     gradeResult = "C";
+// } else if (grade >= 60) {
+//     gradeResult = "D";
+// } else {
+//     gradeResult = "F";
+// }
+
+// console.log(gradeResult);
+
+
+// Switch Statement
+// let number = 5;
+// switch (number) {
+// 	case 1:
+// 		console.log("case " + number);
+// 		break;
+// 	case 2:
+// 		console.log("case " + number);
+// 		break;
+// 	case 3:
+// 		console.log("case " + number);
+// 		break;
+// 	case 4:
+// 		console.log("case " + number);
+// 		break;
+// 	case 5:
+// 		console.log("case " + number);
+// 		break;
+
+// 	default:
+//         console.log("Give me a number dude!")
+// 		break;
+// }
+
+// // Nested Case
+// let num2 = 6;
+// switch (num2) {
+//     case 1:
+//     case 2:
+//     case 3:
+//         console.log("case " + num2);
+//         break;
+//     case 4:
+//     case 5:
+//     case 6:
+//         console.log("case " + num2);
+//         break;
+//     default:
+//             console.log("Gimme somth'n dude!");
+//         break;
+// }
+
+
+// Ternary operator
+// let lighting = "dark";
+// lighting === "dark" ? "It's dark" : "It's light";
+// console.log(lighting);
+
+// Exercise 1
+
+let letter = "A";
+
+switch (letter) {
+    case "A":
+    case "E":
+    case "I":
+    case "O":
+    case "U":
+        console.log(letter + " is a vowel");
+        break;
+    default:
+        console.log(Letter + " Is not a Vowel");
+        break;
+}
+// Exercise 2 Terenayr Operator
+
+let number = 3;
+
+oddEven = number % 2 === 0 ? "It's even" : "It's odd";
+console.log(oddEven);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
