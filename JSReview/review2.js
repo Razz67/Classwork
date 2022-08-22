6f809dc974d0b9699ac844b989e3afb79ccdfc43
Razz67 <srasberry00@gmail.com>
2022-08-22 16:03:27 -0400

    Wild Wild West answered

--- JSReview/review2.js
+++ JSReview/review2.js
@@ -2,29 +2,43 @@
 
 // Part II JavaScript Reps
 // Easy Going
-for (let i = 0; i <= 20; i++) {
-    console.log(i);  
-}
+// for (let i = 0; i <= 20; i++) {
+//     console.log(i);  
+// }
 
-// Get Even
-for (let i = 0; i <= 200; i++) {
-    if (i % 2 === 0) {
-        console.log(i);
-    }
-}
+// // Get Even
+// for (let i = 0; i <= 200; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i);
+//     }
+// }
 
-//  fizzbuzz
-const fizzbuzz = function(num) {
-    for (let i = 1; i <= num; i++) {
-        if (i % 3 === 0 && i % 5 === 0) {
-        console.log("FizzBuzz");
-        } else if (i % 3 === 0) {Fi
-        console.log("Fizz");
-        } else if (i % 5 === 0) {
-        console.log("Buzz");
-        } else {
-        console.log(i);
-        }
-    }
-    }
-    fizzbuzz(30);
+// //  fizzbuzz
+// const fizzbuzz = function(num) {
+//     for (let i = 1; i <= num; i++) {
+//         if (i % 3 === 0 && i % 5 === 0) {
+//         console.log("FizzBuzz");
+//         } else if (i % 3 === 0) {Fi
+//         console.log("Fizz");
+//         } else if (i % 5 === 0) {
+//         console.log("Buzz");
+//         } else {
+//         console.log(i);
+//         }
+//     }
+//     }
+//     fizzbuzz(30);
+
+
+// Wild Wild Life
+// const wolfy = ["Wolfy", "Wolf", 16, "Yukon Territory"];
+// const sharky = ["Sharky", "shark", 20, "Left Coast"];
+// const plantee = ["Plantee", "plant", 5000, "Mordor"];
+// const porgee = ["Porgee", "porg", 186, "Ahch-To"];
+// const dart = ["D'Art", "Doberman dog", 2, "Upside Down"];
+
+// plantee[2]++;
+// wolfy[3] = "Gotham City";
+// dart.push("Hawkins");
+// wolfy[0] = "Gameboy";
+// console.log(wolfy);
