72f431414a3642a90f698a3b828c4fbfdd49d0f5
Razz67 <srasberry00@gmail.com>
2022-08-22 15:42:24 -0400

    Get even answered

--- JSReview/review2.js
+++ JSReview/review2.js
@@ -5,6 +5,14 @@
 for (let i = 0; i <= 20; i++) {
     console.log(i);  
 }
+
+// Get Even
+for (let i = 0; i <= 200; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+}
+
 //  fizzbuzz for loop exercise
 
 const fizzbuzz = function(num) {
