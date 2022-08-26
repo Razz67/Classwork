a05db382e9d46e0ff8120c0d189f2df360eb361b
Razz67 <srasberry00@gmail.com>
2022-08-26 15:06:15 -0400

    Finished 5.2.3

--- DOM-LAB/dom.js
+++ DOM-LAB/dom.js
@@ -28,7 +28,7 @@ let menuLinks = [
 	},
 ];
 
-// Set showingSubMenu to false
+// 5.1.1 Set showingSubMenu to false
 let showingSubMenu = false;
 
 let mainEl = document.querySelector("main");
@@ -70,3 +70,25 @@ subMenuEl.style.backgroundColor = "var(--sub-menu-bg)";
 
 // 4.3 Add class "flex-around" to subMenuEl
 subMenuEl.classList.add("flex-around");
+
+// 4.4 Set subMenuEl position to absolute
+subMenuEl.style.position = "absolute";
+
+// 4.5 Set sumMenuEl top property to top 0
+subMenuEl.style.top = "0";
+
+// 5.1 Select all a tags in topMenuEl and assign to topMenuLinks
+let topMenuLinks = topMenuEl.querySelectorAll("a");
+
+// 5.2 Attach a delegated event listener to topMenuEl
+topMenuEl.addEventListener("click", (event) => {
+
+// 5.2.1 call preventDefault on event
+event.preventDefault();
+
+// 5.2.2 If event target is not an a tag, return
+if (event.target.tagName !== "A") return;
+
+// 5.2.3 console log event target
+console.log(event.target);
+});
