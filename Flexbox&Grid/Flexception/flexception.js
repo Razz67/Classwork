a865681717726d03d74566c81619e6ae36d3595a
Razz67 <srasberry00@gmail.com>
2022-08-30 14:11:32 -0400

    Finished Grid Art

---
+++ Flexbox&Grid/Flexception/flexception.js
@@ -0,0 +1,7 @@
+let h1 = document.createElement("h1");
+h1.appendChild(document.createTextNode("Flex-ception"))
+document.body.appendChild(h1);
+
+h1.style.color = "blue";
+h1.style.fontSize = "70px";
+h1.style.textAlign = "center";
