9804364b048c2b698c6ed97e90473613c7e7494c
Razz67 <srasberry00@gmail.com>
2022-09-27 12:20:26 -0400

    Add Mong DB

--- Node-Practice/express-routes/server.js
+++ Node-Practice/express-routes/server.js
@@ -1,9 +1,10 @@
 // Load express
 const express = require('express')
+const mongoose = require("mongoose")
 
 // Identify our port
 const port = process.env.PORT || 3000;
-require("dotenv").config()
+require("dotenv").config();
 
 // Creates our express app (object)
 const app = express()
@@ -13,6 +14,12 @@ app.set('view engine', 'jsx');
 app.engine('jsx', require('express-react-views').createEngine());
 
 
+
+// Bring in our packaged routes
+const fruitRoutes = require('./routes/fruitRoute')
+const meatRoutes = require('./routes/meatRoute')
+const vegetableRoutes = require('./routes/vegetableRoute')
+
 // Middleware
 app.use(express.urlencoded({extended:false}))
 app.use(express.static("public"))
@@ -20,12 +27,11 @@ app.use("/fruits", fruitRoutes);
 app.use("/meats", meatRoutes);
 app.use("/vegetables", vegetableRoutes);
 
-// Bring in our packaged routes
-const fruitRoutes = require('./routes/fruitRoute')
-const meatRoutes = require('./routes/meatRoute')
-const vegetableRoutes = require('./routes/vegetableRoute')
 
-
+mongoose.connect(process.env.MONGO_DB);
+mongoose.connection.once("open", () => {
+    console.log("Connected to MongoDb");
+})
 
 // Listen to port
 app.listen(port, () => {
