6ec465ebb1259e516504b5d5cd0d5418dfdf1892
Razz67 <srasberry00@gmail.com>
2022-09-30 15:47:36 -0400

    complete

--- Node-Practice/experess-practice/server.js
+++ Node-Practice/experess-practice/server.js
@@ -1,74 +1,44 @@
 // Load Express
 const express = require('express');
+const fs = require('fs');
 
-// create express app
+// Create an instance of Express
 const app = express();
 
+app.engine("madeline", (filePath, options, callback) => {
+	fs.readFile(filePath, (err, content) => {
+		if (err) return callback(err);
+
+		// This is an extremely simple template engine
+		const rendered = content
+			.toString()
+			.replace('#title#', '<title>' + options.title + '</title>')
+			.replace('#message#', '<h1>' +options.title + '</h1>').replace('#content#', '<div>' + options.content + '</div>')
+        return callback(null, rendered);
+	});
+});
+
+app.set("views", "./views"); // specify the views directory
+app.set("view engine", "madeline"); // register the template engine
 
 // Configure the app (app.set)
-const fs = require('fs') // this engine requires the fs module like we did Saturday
-app.engine('madeline', (filePath, options, callback) => { // define the view engine called hypatia
-  fs.readFile(filePath, (err, content) => {
-    if (err) return callback(err)
-    // this is an extremely simple view engine we'll be more complex later
-    const rendered = content.toString()
-      .replace('#title#', '<title>' + options.title + '</title>')
-      .replace('#message#', '<h1>' + options.message + '</h1>')
-      .replace('#content#','<div>'+ options.content + '</div>' )
-
-    return callback(null, rendered)
-  })
-});
-
-app.set('views', './views') // specify the views directory
-app.set('view engine', 'madeline') // register the hypatia view engine
-
-
-// Define a root route
-// The first parameter ('/',) is the path, the second (req, res) is a callback function
-
-app.get("/", (req, res) => {
-	res.render("template", {
-		title: "Hey",
-		message: "Hello there!",
-		content: "I am the Boss Ricky Ross",
-	});
-});
-
-app.get("/about-me", (req, res) => {
-	res.render("template", {
-		title: "Hey",
-		message: "Rick Ross!",
-		content: "The most underated Rapper in the game",
-	});
-});
-
-app.get("/another-one", (req, res) => {
-	res.render("template", {
-		title: "We The Best",
-		message: "Who!",
-		content:
-			"We Taking Over, Major Key Alert, Yall know who it is, All I do is win",
-	});
-});
-
 
 // Mount middleware (app.use)
 
+// Mount routes
+app.get('/',  (req, res) => {
+    res.render('template', { title: 'Hey', message: 'Hello there!', content: 'I am the Boss Ricky Ross'});
+});
 
+app.get('/about-me',  (req, res) => {
+    res.render('template', { title: 'Hey', message: 'Rick Ross', content: 'The most underrated rapper of all time'});
+});
 
+app.get('/another-one',  (req, res) => {
+    res.render('template', { title: 'We The Best', message: 'Who', content: 'We taking over, Major Key Alert, Yall know who it is, All I do is win'});
+});
 
-// Define a root route
-// The first parameter ('/',) is the path, the second (req, res) is a callback function
-// app.get('/', (req, res) => {
-//     res.send('<h1>Hello Razz!</h1>');
-//     });
-
-// app.get('/home', (req, res) => {
-// res.send('<h1>Home Page</h1>');
-// });
-
-// Listen to port 3000
+// Listen on port 3000
 app.listen(3000, () => {
-    console.log('Server is up on port 3000');
-    });
+    console.log('Listening on port 3000');
+});
