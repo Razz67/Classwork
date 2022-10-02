1e8df126bb4395bfb858853c24df7e4ccfdd31c6
Razz67 <srasberry00@gmail.com>
2022-10-01 21:00:37 -0400

    Create New route

--- Node-Practice/fruits/server.js
+++ Node-Practice/fruits/server.js
@@ -2,16 +2,55 @@ const express = require("express");
 const app = express();
 const fruits = require("./models/fruits.js");
 
+// Set up view engine
+app.set("view engine", "jsx");
+app.engine("jsx", require("express-react-views").createEngine());
 
+// Middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static("public"));
+
+
+// ********** INDUCES (Index, New, Delete, Update, Create, Edit, Show **********
 // Root route
-app.get("/fruits", (req, res) => {
-	res.send(fruits);
+app.get("/", (req, res) => {
+	res.send('This is the root route');
 });
+
+// Index route
+app.get("/fruits", (req, res) => {
+	res.render("Index", {fruits: fruits});
+});
+
+// Create New route
+app.get("/fruits/new", (req, res) => {
+	res.render("New");
+});
+
+// Create route
+app.post("/fruits", (req, res) => {
+	
+	if (req.body.readyToEat === "on" || req.body.readyToEat === "true") {
+		req.body.readyToEat = true;
+	} else {
+		req.body.readyToEat = false;
+	}
+	fruits.push(req.body);
+	console.log(req.body);
+	res.redirect("/fruits");  // send the user to the index page
+});
+
+
 
 // Show route
 app.get("/fruits/:indexOfFruitsArray", (req, res) => {
-	res.send(fruits[req.params.indexOfFruitsArray]);
+	res.render('Show', {fruit: fruits[req.params.indexOfFruitsArray]  // look inside the views folder for Show.jsx and pass in the html
+	});
 });
+
+
+
+
 
 app.listen(3000, () => {
 	console.log("Listening on port 3000");
---
+++ Node-Practice/fruits/views/Show.jsx
@@ -0,0 +1,16 @@
+const React = require('react');
+class Show extends React.Component {
+    render() {
+        const fruit = this.props.fruit;
+        return (
+            <div>
+            <h1>
+                <h1>Show Page</h1>
+                {/* can't use a if=else or for loops in jsx so we use a ternary operator */}
+                The {fruit.name} is {fruit.color} and is {fruit.readyToEat ? 'ready to eat' : 'not ready to eat'}
+            </h1>
+            </div>
+        )
+    }
+};
+module.exports = Show;
